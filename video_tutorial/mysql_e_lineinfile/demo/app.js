ca6664b44521e34bc48a6c0049f49ba95c82d43a
Pierangelo <pierangelo1982@gmail.com>
2020-01-17 20:59:07 +0100

    mysql_e_lineinfile

--- video_tutorial/mysql_e_lineinfile/demo/app.js
+++ video_tutorial/mysql_e_lineinfile/demo/app.js
@@ -1,18 +1,60 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
 const os = require('os');
 
+//const hostname = os.hostname();
 const hostname = '0.0.0.0';
 
-//const hostname = os.hostname();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode == 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`Host: ${os.hostname()}`)
+// mysql
+var mysql = require('mysql');
+
+const connection = new mysql.createConnection({
+    host: '192.168.60.1',
+    user: 'demo',
+    password: 'demo'
+    //database: 'todoapp'
 });
 
-server.listen(port, hostname, () => {
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host: '192.168.60.1',
+    user: 'demo',
+    password: 'demo'
+  });
+
+app.get('/', function (req, res) {
+    res.send(`Host: ${os.hostname()}\n`);
+});
+
+app.get('/db', function (req, res) {        
+        
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            res.send(`DB NON CONNESSO: ${os.hostname()}\n`);
+        } // not connected!
+        else {
+            // Use the connection
+            connection.query('select * from information_schema.processlist;', function (error, results, fields) {
+                // When done with the connection, release it.
+                connection.release();
+    
+                res.send(`DB CONNESSO SU: ${os.hostname()}\n`);
+                // Handle error after the release.
+                if (error) {
+                    res.send(`DB NON CONNESSO SU: ${os.hostname()}\n`);
+                    // Don't use the connection here, it has been returned to the pool.
+                }
+            });
+        }
+      });
+});
+
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
